f19fea0ab128415040b16f467dc43af1eef117fe
gamescomputersplay <gamescomputerplay@gmail.com>
2023-02-08 12:40:41 +0800

    small fix

--- downloader.py
+++ downloader.py
@@ -121,7 +121,7 @@ class Downloader:
             minutes = int((secs - hours * 3600) // 60)
             seconds = int(secs - hours * 3600 - minutes * 60)
 
-            return f"{str(hours)}: " + \
+            return f"{str(hours)}:" + \
                    f"{str(minutes).zfill(2)}:" + \
                    f"{str(seconds).zfill(2)}"
 
